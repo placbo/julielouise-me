a9d95c6a12d3c8d37a5390ca828ff6357301f066
pcb <pcb@unit.no>
2020-12-13 23:22:48 +0100

    styling

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-var confettiSettings = { target: 'my-canvas' };
+var confettiSettings = { target: "my-canvas", size: "3", max: "150" };
 var confetti = new ConfettiGenerator(confettiSettings);
 confetti.render();
